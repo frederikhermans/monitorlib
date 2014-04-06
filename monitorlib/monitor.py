93ce8b79e408751e4c4bc4ac9c29fd1d8f203259
Frederik Hermans <frederik.ac.hermans@gmail.com>
2014-04-06 15:00:13 +0200

    Add method for loggin log_events to a file.

--- monitorlib/monitor.py
+++ monitorlib/monitor.py
@@ -3,6 +3,8 @@
 from sitemanagerhandle import SiteManagerHandle
 import nodelist
 
+_LOG_DATE_FMT = '%Y-%m-%d %H:%M:%S'
+
 class Monitor(object):
     '''The Monitor class represents a handle to the testbed.'''
     sitemgr = None
@@ -34,3 +36,34 @@ class Monitor(object):
     def remove_listener(self, regex, callback):
         '''Remove a listener.'''
         self.listeners.remove((regex, callback))
+
+    def log_to_file(self, filename, mode='w'):
+        '''Write all incoming log events to `filename`'''
+        assert mode == 'a' or mode == 'w', 'Mode must be _a_ppend or _w_rite.'
+
+        def callback(_, match):
+            '''Write one log event to `filename`'''
+            groups = match.groupdict()
+            now = datetime.now().strftime(_LOG_DATE_FMT)
+
+            ip = groups['ip']
+            port = groups['port']
+
+            # Look up the node's rime and gid
+            gid = '??'
+            rime = '??'
+            node = self.nodes.select(ip=ip, port=port)
+            if len(node) == 1:
+                gid = node[0].gid
+                if hasattr(node[0], 'rime'):
+                    rime = node[0].rime
+
+            # Write the line
+            f.write('{} {} {} {}\n'.format(now, gid, rime, groups['logline']))
+            f.flush()
+
+        #  Open the file, register the callback
+        f = open(filename, mode)
+        re_le_all = re.compile(r'(?P<at>\d+) (?P<ip>[^:]+):(?P<port>[^ ]+) ' +
+                               r'LE_ALL (?P<logline>.*)')
+        self.add_listener(re_le_all, callback)
